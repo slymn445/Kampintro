76bd099ef508504bf5c9a4b4ad8e3306858373f0
slymn <slymn@COMOLOKKKO>
2021-01-17 18:58:20 +0300

    16 ocak ders sonu

---
+++ Kampintro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kampintro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		double dolarDun = 7.35;
+		double dolarBugun = 7.45;
+
+		if (dolarDun>dolarBugun)
+
+		{
+
+			Console.WriteLine("azalış");
+		}
+	    else if (dolarDun<dolarBugun)
+			{
+			Console.WriteLine("artış");
+			}
+		else
+		{
+			Console.WriteLine("değişmedi");
+		}
+	        
+	}					
+}
